baec2d9692ba17aefc7b324b0b5ae2970af02295
agis <agistt@gmail.com>
2020-01-30 21:02:39 +0200

    fixes + userstory 4

--- src/app/bugs/bugs-list/update/update.component.ts
+++ src/app/bugs/bugs-list/update/update.component.ts
@@ -1,70 +1,89 @@
 import { Component, OnInit } from "@angular/core";
-import { Validators, FormBuilder, FormGroup } from "@angular/forms";
-import { PostmanCallService } from '../../postman-call.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Validators, FormBuilder, FormGroup, FormArray } from "@angular/forms";
+import { PostmanCallService } from "../../postman-call.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { tap } from "rxjs/operators";
+import { Bug } from "src/app/model/bug";
 
 @Component({
   selector: "app-update",
-  templateUrl: './update.component.html',
-  styleUrls: ['./update.component.css']
+  templateUrl: "./update.component.html",
+  styleUrls: ["./update.component.css"]
 })
 export class UpdateComponent implements OnInit {
   public form: FormGroup;
 
   bug: any;
   title: string;
-  priority: string[] = ['Minor', 'Major', 'Critical'];
-  reporter: string[] = ['QA', 'PO', 'DEV'];
-  status: string[] =  ['Ready for test ', 'Done', 'Rejeted'];;
+  priority: any[] = [
+    { id: "1", name: "Minor" },
+    { id: "2", name: "Major" },
+    { id: "3", name: "Critical" }
+  ];
+  reporter: string[] = ["QA", "PO", "DEV"];
+  status: string[] = ["Ready for test ", "Done", "Rejected"];
   description: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private service: PostmanCallService,
     private activatedRoute: ActivatedRoute,
-    private router : Router
+    private router: Router
   ) {}
 
   ngOnInit() {
+    this.buildForm();
+
     if (this.activatedRoute.snapshot.params["id"] != null) {
       this.service
         .getBugById(this.activatedRoute.snapshot.params["id"])
-        .subscribe(data => {
-          (this.bug = data),
-            (this.form = this.formBuilder.group({
-              title: [this.bug.title, Validators.required],
-              priority: [this.bug.priority, Validators.required],
-              reporter: [this.bug.reporter, Validators.required],
-              description: [this.bug.description, Validators.required],
-              status: [this.bug.status, Validators.required]
-            }));
+        .subscribe((data: any) => {
+          this.form.patchValue(data);
+          this.fillInComments(data);
+          this.bug = data;
         });
-    } else {
-      this.form = this.formBuilder.group({
-        title: ["", Validators.required],
-        priority: ["", Validators.required],
-        reporter: ["", Validators.required],
-        status: ["", Validators.required],
-        description: ["", Validators.required]
-      });
     }
   }
 
   onSubmit(id: string) {
-    if (id == null) {
-      if (this.form.invalid) {
-        return;
-      } else {
-        this.service.addBug(this.form.value).subscribe();
-      }
-    } else {
-      if (this.form.invalid) {
-        return;
-      } else {
-        this.service.updateBug(this.form.value, id).subscribe();
-      }
+    if (this.form.invalid) {
+      return;
     }
-    this.router.navigate(['/']);
+
+    const actionToInvoke = id
+      ? this.service.updateBug(this.form.value, id)
+      : this.service.addBug(this.form.value);
+
+    actionToInvoke.pipe(tap(() => this.router.navigate([""]))).subscribe();
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      title: ["", Validators.required],
+      priority: ["", Validators.required],
+      reporter: ["", Validators.required],
+      status: ["", Validators.required],
+      description: ["", Validators.required],
+      comments: this.formBuilder.array([this.commentArrayItem()])
+    });
+  }
+
+  private commentArrayItem(comment?) {
+    return this.formBuilder.group({
+      reporter: [comment && comment.reporter],
+      description: [comment && comment.description]
+    });
+  }
+
+  private fillInComments(data: any) {
+    if (data.comments) {
+      (this.form.get("comments") as FormArray).removeAt(0);
+      data.comments.forEach(comment => {
+        const commentAsFormGroup = this.commentArrayItem(comment);
+        (this.form.get("comments") as FormArray).push(commentAsFormGroup);
+      });
+    }
+  }
 }
+
+
